7cb64315adf953a70ed1f809ade19cb7a19a10b4
Ruska Beridze <rberidze@ambergroup.ge>
2021-09-27 12:51:04 +0400

    challenges #3 and #4

--- challenges-14-oop.js
+++ challenges-14-oop.js
@@ -54,3 +54,57 @@ ford.accelerate();
 ford.break();
 ford.speedUS = 100;
 console.log(ford.speed, ford.speedUS);
+
+//Challenge #3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+
+console.log(tesla);
+tesla.chargeBattery(90);
+tesla.accelerate();
+
+//Challenge #4
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+    );
+    return this;
+  }
+
+  chargeBattery() {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  break() {
+    this.speed -= 10;
+    this.#charge += 1;
+  }
+}

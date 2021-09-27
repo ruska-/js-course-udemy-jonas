9ffb46c5778a7c4f5d140f79a8f14580cdac8a6c
Ruska Beridze <rberidze@ambergroup.ge>
2021-09-27 12:00:53 +0400

    challenge #2

--- challenges-14-oop.js
+++ challenges-14-oop.js
@@ -23,3 +23,34 @@ car2.accelerate();
 car2.break();
 
 //Challenge #2
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    console.log(this.make, (this.speed += 10));
+  }
+
+  break() {
+    console.log(this.make, (this.speed -= 5));
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speedMiH) {
+    this.speed = speedMiH * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+ford.accelerate();
+ford.break();
+ford.accelerate();
+ford.break();
+ford.speedUS = 100;
+console.log(ford.speed, ford.speedUS);
